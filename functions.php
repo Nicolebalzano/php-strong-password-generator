cb5ea3afca2a1c069582464bd36de4cdd683a0bf
Nicolebalzano <nicole.balzano03@gmail.com>
2025-08-08 16:09:35 +0200

    feat:logica spostata nel file functions.php

---
+++ functions.php
@@ -0,0 +1,20 @@
+  <?php
+    if(isset($_GET["length"])) {
+        $length = $_GET['length'];
+    function generate_password($length){
+        $characters = "QWERTYUIOPLKJHGFDSAZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890$!?£_.";
+        $password = "";
+        for($i = 0; $i < $length; $i++){
+            $index = random_int(0, strlen($characters) -1);
+            $password .= $characters[$index];
+          
+        } 
+         return $password;
+    }
+    $password = generate_password($length);
+     echo "La tua password è: " . $password;
+    } else{
+        echo "setta la lunghezza";
+    }
+    
+    ?>
--- index.php
+++ index.php
@@ -12,26 +12,9 @@
 </label>
 <button type="submit">Invia</button>
     </form>
-    <?php
-    if(isset($_GET["length"])) {
-        $length = $_GET['length'];
-    function generate_password($length){
-        $characters = "QWERTYUIOPLKJHGFDSAZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890$!?£_.";
-        $password = "";
-        for($i = 0; $i < $length; $i++){
-            $index = random_int(0, strlen($characters) -1);
-            $password .= $characters[$index];
-          
-        } 
-         return $password;
-    }
-    $password = generate_password($length);
-     echo "La tua password è: " . $password;
-    } else{
-        echo "setta la lunghezza";
-    }
-    
-    ?>
+<?php
+require_once './functions.php';
+?>
    
 </body>
 </html>
